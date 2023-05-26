5b7a521f13d7c2092d5568eff2086b41021b9c5d
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-05-26 22:04:34 +0100

    change yml config

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { random } from "lodash";
+// import { random } from "lodash";
 
 export const handler = () => {
   return {
     statusCode: 200,
-    body: JSON.stringify(random(999)),
+    body: JSON.stringify(Math.random() * 999),
   };
 
 
