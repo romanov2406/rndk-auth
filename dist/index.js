92bbceaec020e75f4e3ed9569ad0b4c761cffd14
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-05-26 22:16:59 +0100

    change yml config

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
+"use strict";
 // import { random } from "lodash";
-export const handler = () => {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+var handler = function () {
     return {
         statusCode: 200,
         body: JSON.stringify(Math.random() * 999),
     };
 };
+exports.handler = handler;
 //# sourceMappingURL=index.js.map
