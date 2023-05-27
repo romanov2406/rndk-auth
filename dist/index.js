164f34e0d4df9edf5e5a198d3ad025fa597dd499
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-05-27 15:31:03 +0100

    change handler response

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const handler = async () => {
+    return {
+        statusCode: "200",
+        body: JSON.stringify(Math.random() * 999),
+    };
+};
+exports.handler = handler;
+// tsc index.ts
+// mv index.js dist
