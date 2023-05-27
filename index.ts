1e4ba85a64168cc51b81e8e824da998095150f51
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-05-27 15:30:50 +0100

    change handler response

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-// import { random } from "lodash";
 
-export const handler = () => {
+export const handler = async () => {
   return {
-    "statusCode": "200",
-    "body": JSON.stringify(Math.random() * 999),
+    statusCode: "200",
+    body: JSON.stringify(Math.random() * 999) + 'Romeo',
   };
 };
+
+
+// tsc index.ts
+// mv index.js dist
