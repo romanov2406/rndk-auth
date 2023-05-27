aa51ba0731c39d317343f054447c2040bc876bab
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-05-27 15:56:18 +0100

    add types

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 import * as _ from "lodash";
 import { User } from "./controllers/user";
 
-export const handler = async () => {
-  console.log("hello world");
-  console.log("Name ivan");
-
+export const handler = (): { [k: string]: string } => {
   return {
     statusCode: "200",
     body: JSON.stringify(_.random(999) + " " + "Romeo" + " " + User()),
